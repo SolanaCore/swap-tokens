7c102b23acb6c26a444c8f5d9a3252ca279632ff
DhruvWebDev <artificialintelligencehub35@gmail.com>
2025-06-18 11:07:34 +0000

    removed code scanning because of the conflict and also add anchor_lang

--- swap/programs/swap/src/state/offer.rs
+++ swap/programs/swap/src/state/offer.rs
@@ -6,7 +6,7 @@ pub struct Offer {
     bump: u8,
     proposer: Pubkey,
     token_0_amount: u64,
-    token_1_amount: u64,
+    token_1_amount: u64, 
     token_0_mint: Pubkey,
     token_1_mint: Pubkey,
     timestamp: i64,
