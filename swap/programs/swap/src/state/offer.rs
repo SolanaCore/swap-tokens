3517a093808f50557a4f7b4b36369ae33f4d1f9d
DhruvWebDev <artificialintelligencehub35@gmail.com>
2025-06-18 10:36:45 +0000

    feat: created offer account

---
+++ swap/programs/swap/src/state/offer.rs
@@ -0,0 +1,15 @@
+#[account]
+#[derive(InitSpace, Clone, Debug)]
+pub struct Offer {
+    bump: u8,
+    proposer: Pubkey,
+    token_0_amount: u64,
+    token_1_amount: u64,
+    token_0_mint: Pubkey,
+    token_1_mint: Pubkey,
+    timestamp: i64,
+    is_active: bool,
+    is_fulfilled: bool,
+    is_cancelled: bool,
+    offer_id: u64,
+}
