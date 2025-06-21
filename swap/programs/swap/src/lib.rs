5dc1d790eca45e27ce7084619c333635026c5bb8
DhruvWebDev <artificialintelligencehub35@gmail.com>
2025-06-21 13:59:31 +0000

    completed writing the src/lib.rs

--- swap/programs/swap/src/lib.rs
+++ swap/programs/swap/src/lib.rs
@@ -5,31 +5,48 @@ pub mod state;
 pub mod utils;
 use anchor_lang::prelude::*;
 
-pub use constants::*;
-pub use instructions::*;
-pub use state::*;
-pub use utils::*;
-pub use error::ErrorCode;
+ use constants::*;
+ use instructions::*;
+ use state::*;
+ use utils::*;
+ use error::ErrorCode;
 
 declare_id!("Swap3sqTyxNUHeUSNg5M6dmCmvsuMdRNdrqm7o2Khrt");
 
 #[program]
 pub mod swap {
-    #[allow(unused_imports)]
     use super::*;
-    // pub fn create_offer()-> Result<()>{
+    
+ pub fn create_offer(mut ctx: Context<CreateOffer>, token_0_amount: u64, token_1_amount: u64) -> Result<()> {
+        instructions::create_offer(&mut ctx, token_0_amount, token_1_amount)?;
+        Ok(())
+    }
 
-    // }
+    pub fn accept_offer(ctx: Context<AcceptOffer>) -> Result<()>{
+        instructions::accept_offer(ctx)?;
+        Ok(())
+    }
 
-    // pub fn accept_offer() -> Result<()>{
+    pub fn edit_offer(mut ctx: Context<EditOffer>,token_0_amount: u64, token_1_amount: u64) -> Result<()>{
+        instructions::edit_offer(&mut ctx, token_0_amount, token_1_amount)?;
+        Ok(())
+    }
 
-    // }
-
-    // pub fn edit_offer() -> Result<()>{
-
-    // }
-
-    // pub fn cancel_offer() -> Result<()>{
-
-    // }
+    pub fn cancel_offer(_ctx: Context<CancelOffer>) -> Result<()>{
+        instructions::cancel_offer(&_ctx)?;
+        Ok(())
+    }
 }
+
+/*
+error[E0596]: cannot borrow `ctx` as mutable, as it is not declared as mutable
+  --> programs/swap/src/lib.rs:21:36
+   |
+21 |         instructions::create_offer(&mut ctx, token_0_amount, token_1_amount)?;
+   |                                    ^^^^^^^^ cannot borrow as mutable
+   |
+help: consider changing this to be mutable
+   |
+20 |  pub fn create_offer(mut ctx: Context<CreateOffer>, token_0_amount: u64, token_1_amount: u64) -> Result<()> {
+   |                      +++
+*/
